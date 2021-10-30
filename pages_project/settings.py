2d2ba88dbe85b053f306b807b117c055174b1187
Alex Kuplays <alexkuplays@gmail.com>
2021-10-30 21:45:38 +0700

    removed secret key

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7+qkv!5n+s_#p81ez&7fu9os&%$3w_df-+v)rq4s+nfsgi%4ta'
+SECRET_KEY = 'DELETED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
